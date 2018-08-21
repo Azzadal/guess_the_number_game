a3f4fb427735b8d8d38b7f00057a996fcc2c4dc0
Azzadal <42366640+Azzadal@users.noreply.github.com>
2018-08-22 00:07:11 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,20 @@
+﻿var rand;
+function randomInteger(min, max) {
+	rand = min - 0.5 + Math.random() * (max-min+1)
+	rand = Math.round(rand);
+	return rand;
+	}
+	randomInteger(0, 5);
+
+
+function change() {
+	var x = document.getElementById("name");
+	var answer = Math.sqrt(rand);
+	if(x.value<rand) {
+		document.getElementById("a").innerHTML = ("мало");
+	} else if(x.value>rand) {
+		document.getElementById("a").innerHTML = ("много");
+	} else{
+		document.getElementById("a").innerHTML = ("норм");
+	}	
+}
