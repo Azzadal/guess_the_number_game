a01dac7e67ef08e78befdef6ab4f8636e660b175
Azzadal <42366640+Azzadal@users.noreply.github.com>
2018-08-28 22:01:05 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,20 +1,43 @@
-﻿var rand;
+﻿
 function randomInteger(min, max) {
-	rand = min - 0.5 + Math.random() * (max-min+1)
-	rand = Math.round(rand);
-	return rand;
+	var rand_numb;
+	rand_numb = min - 0.5 + Math.random() * (max-min+1)
+	rand_numb = Math.round(rand_numb);
+	return rand_numb;
 	}
-	randomInteger(0, 5);
+	rand_numb = randomInteger(0, 10);
+
 
 
 function change() {
-	var x = document.getElementById("name");
-	var answer = Math.sqrt(rand);
-	if(x.value<rand) {
-		document.getElementById("a").innerHTML = ("мало");
-	} else if(x.value>rand) {
-		document.getElementById("a").innerHTML = ("много");
-	} else{
-		document.getElementById("a").innerHTML = ("норм");
-	}	
+	var user_input = document.getElementById("user_input");
+	response_label = document.getElementById("response_label");
+	var square_root = Math.sqrt(user_input.value);
+	sqrt = document.getElementById("sqrt");
+	right_answer = document.getElementById("right_answer");
+	if(user_input.value<rand_numb) {	
+		response_label.innerHTML = "мало";
+		sqrt.innerHTML = ("&#8730: " + square_root);
+		right_answer.innerHTML = "Попытка№ "+counter();
+	} 
+	else if(user_input.value>rand_numb) {
+		response_label.innerHTML = "много";
+		sqrt.innerHTML = ("&#8730: " + square_root);
+		right_answer.innerHTML = "Попытка№ "+counter();
+		
+	}
+	else if(user_input.value==rand_numb){
+		response_label.innerHTML = "норм";
+		sqrt.innerHTML = ("&#8730: " + square_root);
+		right_answer.innerHTML = "Попытка№ "+counter();		
+	}
+    if (user_input.value>10||user_input.value<0) {response_label.innerHTML = "недопустимое значение";}
 }
+
+function makeCounter() {
+var currentCount = 1;
+	return function(){
+		return currentCount++;
+};
+}
+var counter = makeCounter();
