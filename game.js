6c30674f5fea2ef57901edfc4b0eb574fb0ed696
cruvar <sin.tetic89@gmail.com>
2018-09-03 11:43:24 +0700

    Update game.js
    now all spaces. randomInteger function refactor

--- game.js
+++ game.js
@@ -1,34 +1,30 @@
 ﻿function randomInteger(min, max) {
-	var rand_numb;
-	rand_numb = min - 0.5 + Math.random() * (max-min+1)
-	rand_numb = Math.round(rand_numb);
-	return rand_numb;
+    return Math.round(min - 0.5 + Math.random() * (max-min + 1));
 }
 
 function change() {
-	var user_input = document.getElementById("user_input");
-	response_label = document.getElementById("response_label");
-	right_answer = document.getElementById("right_answer");
-    if (user_input.value>10||user_input.value<0) {
-    	response_label.innerHTML = "недопустимое значение";
-    	return;
+    var user_input = document.getElementById("user_input");
+    response_label = document.getElementById("response_label");
+    right_answer = document.getElementById("right_answer");
+    if (user_input.value > 10 || user_input.value < 0) {
+        response_label.innerHTML = "недопустимое значение";
+        return;
     }
-    right_answer.innerHTML = "Попытка№ "+counter();
-	if(user_input.value<rand_numb) {	
-		response_label.innerHTML = "мало";
-	} 
-	else if(user_input.value>rand_numb) {
-		response_label.innerHTML = "много";
-	}
-	else if(user_input.value==rand_numb){
-		document.body.innerHTML = '<span class = "victory">Поздравляем, Вы выиграли!!! <br/><button class="new_game" onclick = "location.reload()">Сыграть заного!</button></span>'
-	}
-
+    right_answer.innerHTML = "Попытка№ " + counter();
+    if(user_input.value < rand_numb) {	
+        response_label.innerHTML = "мало";
+    } 
+    else if(user_input.value > rand_numb) {
+        response_label.innerHTML = "много";
+    }
+    else if(user_input.value == rand_numb) {
+        document.body.innerHTML = '<span class = "victory">Поздравляем, Вы выиграли!!! <br/><button class="new_game" onclick = "location.reload()">Сыграть заного!</button></span>'
+    }
 }
 
 function makeCounter() {
     var currentCount = 1;
-    return function(){
+    return function() {
         return currentCount++;
     };
 }
